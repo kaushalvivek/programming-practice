98e6c8d01c921cde4e38b16114d7fd04fc19363a
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2019-06-20 16:28:40 +0800

    cf1175a

---
+++ cf1175a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int t, i;
+  cin >> t;
+  long long int n, k, count;
+  for (i = 0; i < t; i++)
+  {
+    count = 0;
+    cin >> n >> k;
+    while (n != 0)
+    {
+      if (n % k == 0)
+      {
+        n /= k;
+        count++;
+      }
+      else
+      {
+        n--;
+        count++;
+      }
+    }
+    cout << count << "\n";
+  }
+  return 0;
+}
